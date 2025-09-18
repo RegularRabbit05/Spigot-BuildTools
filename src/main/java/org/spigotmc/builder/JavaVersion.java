a5eeb3cdfd45e956d833db1c12600ce9f428cfc0
md_5 <git@md-5.net>
2025-09-18 21:18:34 +1000

    Java 25 support

--- src/main/java/org/spigotmc/builder/JavaVersion.java
+++ src/main/java/org/spigotmc/builder/JavaVersion.java
@@ -31,6 +31,7 @@ public class JavaVersion
     public static final JavaVersion JAVA_22 = new JavaVersion( "Java 22", 66 );
     public static final JavaVersion JAVA_23 = new JavaVersion( "Java 23", 67 );
     public static final JavaVersion JAVA_24 = new JavaVersion( "Java 24", 68 );
+    public static final JavaVersion JAVA_25 = new JavaVersion( "Java 25", 69 );
     //
     private final String name;
     private final int version;
